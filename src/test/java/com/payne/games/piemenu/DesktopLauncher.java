808c1f5a31670cd9bc4750ab707a309232be0974
payne911 <payne911@users.noreply.github.com>
2020-10-05 22:46:38 -0400

    Re-recorded all the GIFs :)

--- src/test/java/com/payne/games/piemenu/DesktopLauncher.java
+++ src/test/java/com/payne/games/piemenu/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.payne.games.piemenu.testMenu.core.TestsMenu;
 public class DesktopLauncher {
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.samples = 4; // for smoother rendering of arcs
+        config.samples = 8; // for smoother rendering of arcs
 
         /* You can change `TestsMenu` for any of the `codeExamples` example class. */
         new LwjglApplication(new TestsMenu(), config);
